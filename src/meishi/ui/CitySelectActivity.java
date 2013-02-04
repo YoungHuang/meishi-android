c403ff32d72df48a5bd718a4a92b72ece0cd4045
yonghuang <h00138136@163.com>
2013-02-04 14:58:35 +0800

    update

--- src/meishi/ui/CitySelectActivity.java
+++ src/meishi/ui/CitySelectActivity.java
@@ -96,8 +96,8 @@ public class CitySelectActivity extends BaseActivity implements OnItemClickListe
 			public void onSuccess(List<City> cityList) {
 				LinearLayout loadingLayout = (LinearLayout) findViewById(R.id.loadingLayout);
 				loadingLayout.setVisibility(View.GONE);
-				LinearLayout bodyLayout = (LinearLayout) findViewById(R.id.bodyLayout);
-				bodyLayout.setVisibility(View.VISIBLE);
+				ListView hotCityListView = (ListView) findViewById(R.id.hotCityList);
+				hotCityListView.setVisibility(View.VISIBLE);
 				if (cityList != null) {
 					adapter.addMoreItems(cityList);
 				}
